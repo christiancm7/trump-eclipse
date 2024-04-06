72d965299eccdd749961a5df90d1cd7a361b54d8
christian morales <christianmorales@christians-MacBook-Pro.local>
2024-04-06 08:47:55 -0500

    linksg

--- trump-eclipse/src/app/page.tsx
+++ trump-eclipse/src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
 import Button from "@mui/material/Button";
+import { useCallback } from "react";
 
 export default function Home() {
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard
+      .writeText("9N1xPAdaepGHGvpcjw4ZCm4Hw8YrBwp1U9SHPmupDwDT")
+      .then(
+        () => {
+          alert("Contract address copied to clipboard!");
+        },
+        (err) => {
+          console.error("Failed to copy text to clipboard", err);
+        }
+      );
+  }, []);
+
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-between p-24 bg-cover"
@@ -14,7 +29,7 @@ export default function Home() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mb-32">
         <Button
-          href="https://jupiter-terminal.dexscreener.com/?inputMint=CX5m6oZZftX4jbVh7spuHeRoyjwnCsNK868YpXcz2GDc&outputMint=So11111111111111111111111111111111111111112"
+          href="https://jupiter-terminal.dexscreener.com/?inputMint=9N1xPAdaepGHGvpcjw4ZCm4Hw8YrBwp1U9SHPmupDwDT&outputMint=So11111111111111111111111111111111111111112"
           target="_blank"
           variant="contained"
           sx={{
@@ -47,40 +62,7 @@ export default function Home() {
           Buy w/ Jupiter
         </Button>
         <Button
-          variant="contained"
-          href="https://raydium.io/swap/?outputCurrency=CX5m6oZZftX4jbVh7spuHeRoyjwnCsNK868YpXcz2GDc"
-          target="_blank"
-          sx={{
-            borderRadius: "10px",
-            background:
-              "linear-gradient(45deg, #0033A0 30%, #FFFFFF 50%, #B22234 90%)",
-            color: "#FFFFFF",
-            textTransform: "none", // Keeps text in normal case for better readability
-            fontWeight: "bold", // Makes text bold for better visibility
-            boxShadow: `
-      0 0 4px #0033A0, 
-      0 0 8px #B22234, 
-      0 0 12px #FFFFFF`,
-            "&:before": {
-              content: '""',
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              opacity: 0.5,
-            },
-            "&:hover": {
-              background:
-                "linear-gradient(45deg, #002776 30%, #FFFFFF 50%, #992232 90%)",
-            },
-            textShadow: "0px 0px 8px rgba(0, 0, 0, 0.8)", // Adds shadow to text for better readability
-          }}
-        >
-          Buy w/ Radium
-        </Button>
-        <Button
-          href="https://dexscreener.com/solana/byuqkkpooqmxuhbq7x5qvfblxp1qergxs9tpighaee5j"
+          href="https://dexscreener.com/solana/4tc9e6jeqntkqwcueizwbgu723x6hcqxp1maiph3yusy"
           target="_blank"
           variant="contained"
           sx={{
@@ -177,6 +159,30 @@ export default function Home() {
           }}
         >
           Telegram
+        </Button>
+        <Button
+          variant="contained"
+          onClick={copyToClipboard}
+          sx={{
+            borderRadius: "10px",
+            background:
+              "linear-gradient(45deg, #0033A0 30%, #FFFFFF 50%, #B22234 90%)",
+            color: "#FFFFFF",
+            textTransform: "none",
+            fontWeight: "bold",
+            boxShadow: `
+              0 0 4px #0033A0, 
+              0 0 8px #B22234, 
+              0 0 12px #FFFFFF
+            `,
+            "&:hover": {
+              background:
+                "linear-gradient(45deg, #002776 30%, #FFFFFF 50%, #992232 90%)",
+            },
+            textShadow: "0px 0px 8px rgba(0, 0, 0, 0.8)",
+          }}
+        >
+          Contract Address
         </Button>
       </div>
 
